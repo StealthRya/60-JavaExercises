c08bbbf33afaa2452aeccb28d9e42b980d864fc2
StealthRya <rayann07226@gmail.com>
2025-04-03 23:19:19 +0200

    10 Exercises complete

---
+++ src/javareview/Main.java
@@ -0,0 +1,26 @@
+/*Exercise 11
+The photocopy center charges 0.25 DH for the first 10 photocopies, 0.20 DH for the next twenty, and 0.10 DH for more than twenty. Write a Java program that asks the user to enter the number of photocopies made and displays the corresponding invoice. */
+
+
+
+/*Exercise 12
+Write a Java program that asks for a child's age and provides information about their age category, given that the categories are as follows:
+"Chicken from 6 to 7 years old"
+"Pupil from 8 to 9 years old"
+"Minimum from 10 to 11 years old"
+"Cadet after 12 years old." */
+
+
+
+/*Exercise 13
+Write a Java program to display the month in letters based on the number entered on the keyboard. (If the user types 1, the program displays January; if 2, it displays February; if 3, it displays March, etc.) */
+
+
+
+/*Exercise 14
+Write a Java program that displays the message "Good evening" 10 times. Using a while loop. */
+
+
+
+/*Exercise 15
+Write a Python program to calculate the sum S = 1 + 2 + 3 + ... + 10. Using a while loop. */
