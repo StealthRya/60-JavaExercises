fb3867e083f683325c75255bad8439a308dcb030
StealthRya <rayann07226@gmail.com>
2025-04-06 23:56:59 +0200

    22/60 exercises

---
+++ src/javareview/javareview/Main.java
@@ -0,0 +1,152 @@
+/*
+ * Exercise 23
+ * Write a Java program to enter a number and check whether the number is
+ * perfect or not.
+ * A perfect number is a positive integer that is equal to the sum of its proper
+ * positive divisors.
+ * For example: 6 is the first perfect number
+ * The proper divisors of 6 are 1, 2, 3.
+ * Sum of its divisors = 1 + 2 + 3 = 6.
+ * Therefore, 6 is a perfect number.
+ */
+
+/*
+ * Exercise 24
+ * Write a Java program to enter a number and calculate its factorial using a
+ * for loop.
+ * The factorial of a number "n" is the product of all positive integers less
+ * than or equal to n. It is denoted by n!.
+ * For example, factorial of 5! = 1*2*3*4*5 = 120.
+ */
+
+/*
+ * Exercise 25
+ * Write a Java program to display all odd numbers from 1 to n using both for
+ * and while loops.
+ */
+
+/*
+ * Exercise 26
+ * Write a Java program to enter a number from the user and count the number of
+ * digits in the given integer using a loop.
+ */
+
+/*
+ * Exercise 27
+ * Write a Java program to enter a number from the user and find the first and
+ * last digit of the number using a loop.
+ */
+
+/*
+ * Exercise 28
+ * Write a Java program that reverses the digits of an integer N entered by the
+ * user.
+ * For example, if N = 35672, the output should be 27653.
+ */
+
+/*
+ * Exercise 29
+ * Write a Java program to enter a number and calculate the sum of its digits
+ * using a for loop.
+ */
+
+/*
+ * Exercise 30
+ * Write a Java program to enter a number from the user and check whether the
+ * number is a palindrome using a loop.
+ */
+
+/*
+ * Exercise 31
+ * Write a Java program to declare and initialize an array, then input its
+ * elements from the user and display the array.
+ */
+
+/*
+ * Exercise 32
+ * Write a Java program to declare an array, then input its elements from the
+ * user and display all the negative elements.
+ */
+
+/*
+ * Exercise 33
+ * Write a Java program to declare an array, then input its elements from the
+ * user and find the sum of the array elements.
+ */
+
+/*
+ * Exercise 34
+ * Write a Java program to declare an array, then input its elements from the
+ * user and find the maximum and minimum elements in the array.
+ */
+
+/*
+ * Exercise 35
+ * Write a Java program to declare an array, then input its elements from the
+ * user and find the largest and the second largest element in the array.
+ */
+
+/*
+ * Exercise 36
+ * Write a Java program to declare an array, then input its elements from the
+ * user and count the number of even and odd elements in the array.
+ */
+
+/*
+ * Exercise 37
+ * Write a Java program to declare two arrays, then input the elements of the
+ * first array from the user and copy all its elements into the second array.
+ */
+
+/*
+ * Exercise 38
+ * Create a Java program that creates and initializes an array, then inserts an
+ * element at a specified position in this array (from 0 to N-1).
+ * To insert a new element in the array, shift the elements from the given
+ * insertion position one position to the right.
+ */
+
+/*
+ * Exercise 39
+ * Create a Java program that creates and initializes an array, then deletes an
+ * element from this array at a specified position (from 0 to N-1).
+ * To delete an element from the array, shift the elements just after the given
+ * position one position to the left and reduce the array size.
+ */
+
+/*
+ * Exercise 40
+ * Create a Java program that creates and initializes an array, then finds the
+ * frequency of each element in this array.
+ */
+
+/*
+ * Exercise 41
+ * Create a Java program that creates and initializes an array, then displays
+ * all the unique elements of this array.
+ * Hint: use a frequency array.
+ */
+
+/*
+ * Exercise 42
+ * Create a Java program that creates and initializes an array, then counts the
+ * duplicate elements in this array.
+ */
+
+/*
+ * Exercise 43
+ * Create a Java program that creates and initializes an array, then removes the
+ * duplicate elements from this array.
+ */
+
+/*
+ * Exercise 44
+ * Create a Java program that creates and initializes an array, then reverses
+ * this array without using an additional array.
+ */
+
+/*
+ * Exercise 45
+ * Write a Java program that calculates the sum:
+ * S = 1¹ + 2² + 3³ + ........ + n^n
+ */
